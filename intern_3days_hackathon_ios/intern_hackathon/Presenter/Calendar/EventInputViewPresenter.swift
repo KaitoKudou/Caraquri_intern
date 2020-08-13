d9a406007f19566b9224e65080a7d0b6c5129fd2
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-08-13 15:49:20 +0900

    firebaseに書き込むメソッドを追加した

---
+++ intern_3days_hackathon_ios/intern_hackathon/Presenter/Calendar/EventInputViewPresenter.swift
@@ -0,0 +1,31 @@
+//
+//  EventInputViewPresenter.swift
+//  intern_hackathon
+//
+//  Created by 工藤海斗 on 2020/08/13.
+//  Copyright © 2020 caraquri. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class EventInputViewPresenter {
+    
+    let db = Firestore.firestore()
+    
+    // firebaseにイベントを登録する
+    func addEvent(day: Date, month: Date, place: String, memo: String) {
+        var ref: DocumentReference?
+        ref = db.collection("events").addDocument(data: [
+            "date": "\(month)/\(day)",
+            "place": place,
+            "memo": memo
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+    }
+}
