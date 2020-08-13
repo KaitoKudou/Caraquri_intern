2334f8f788c214a429f34b7421d958e31b033f50
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-08-13 15:22:43 +0900

    日付を入力するdetePickerを出した

--- intern_3days_hackathon_ios/intern_hackathon/View/Calendar/EventInputViewController.swift
+++ intern_3days_hackathon_ios/intern_hackathon/View/Calendar/EventInputViewController.swift
@@ -8,23 +8,43 @@
 
 import UIKit
 
-class EventInputViewController: UIViewController {
-
+class EventInputViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        dateTextField.delegate = self
+        setupToolbar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeModal(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    func setupToolbar() {
+        //datepicker上のtoolbarのdoneボタン
+        let toolBar: UIToolbar = UIToolbar()
+        toolBar.sizeToFit()
+        let toolBarBtn = UIBarButtonItem(title: "DONE", style: .plain, target: self, action: #selector(doneBtn))
+        toolBar.items = [toolBarBtn]
+        dateTextField.inputAccessoryView = toolBar
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let datePicker: UIDatePicker = UIDatePicker()
+        datePicker.datePickerMode = UIDatePicker.Mode.date
+        dateTextField.inputView = datePicker
+        datePicker.addTarget(self, action: #selector(datePickerValueChanged(sender:)), for: .valueChanged)
+    }
+    
+    @objc func datePickerValueChanged(sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat  = "MM/dd"
+        dateTextField.text = dateFormatter.string(from: sender.date)
+    }
+    
+    @objc func doneBtn() {
+        dateTextField.resignFirstResponder()
+    }
 }
